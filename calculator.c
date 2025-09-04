e2ffb92c964a019ae19fe92ab1baa5eec0d3441b
brooklynwidz <rish.dash.000000@gmail.com>
2025-09-04 16:43:12 +0530

    second commit

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter number1: ");
+    scanf("%d", &a);
+    printf("enter number2: ");
+    scanf("%d", &b);
+    printf("addition: %d\n", a+b);
+    printf("subtraction: %d\n", a-b);
+    printf("multiplication: %d\n", a*b);
+    printf("Division: %d", a/b);
+}
